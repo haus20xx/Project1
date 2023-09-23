def46f16581e4085d4778a90ff8d3c3249b609df
kenny house <kennysmod@gmail.com>
2023-09-23 15:21:44 -0400

    xhore: formatting

--- src/app/features/multi-select/multi-select/multi-select.component.ts
+++ src/app/features/multi-select/multi-select/multi-select.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core"
-import { FormControl } from "@angular/forms"
-import { map, startWith } from "rxjs"
+import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { map, startWith } from "rxjs";
 
-const BASE_OPTIONS = ["Option1", "Option2", "Option3"]
+const BASE_OPTIONS = ["Option1", "Option2", "Option3"];
 
 @Component({
   selector: "app-multi-select",
@@ -10,37 +10,40 @@ const BASE_OPTIONS = ["Option1", "Option2", "Option3"]
   styleUrls: ["./multi-select.component.scss"],
 })
 export class MultiSelectComponent {
-  public select = new FormControl()
-  public activeFilterChips: Set<string> = new Set()
+  public select = new FormControl();
+  public activeFilterChips: Set<string> = new Set();
 
   public filteredOptions$ = this.select.valueChanges.pipe(
     startWith(""),
     map((value: string) => {
       const FILTERED_OPTIONS = BASE_OPTIONS.filter((opt) => {
-        const lowerOpt = opt.toLowerCase()
-        const lowerValue = value.toLowerCase()
-        const termExists = lowerOpt === lowerValue
-        const chipExists = [...this.activeFilterChips].find(
-          (chip) => chip.toLowerCase() === lowerOpt,
-        )
+        const lowerOpt = opt.toLowerCase();
+        const lowerValue = value.toLowerCase();
+        const termExists = lowerOpt === lowerValue;
+        const chipExists = !(
+          [...this.activeFilterChips].find(
+            (chip) => chip.toLowerCase() === lowerOpt,
+          ) == null
+        );
         //Only present values which are not active as chips nor the typed text
-        return !termExists && !chipExists
-      })
+        return !termExists && !chipExists;
+      });
 
-      return value && !FILTERED_OPTIONS.find((opt) => value === opt)
+      return value.length > 0 &&
+        FILTERED_OPTIONS.find((opt) => value === opt) == null
         ? [value, ...FILTERED_OPTIONS]
-        : FILTERED_OPTIONS
+        : FILTERED_OPTIONS;
     }),
-  )
+  );
 
   public onOptionSelect(option: string): void {
-    this.activeFilterChips.add(option)
-    this.select.setValue("")
+    this.activeFilterChips.add(option);
+    this.select.setValue("");
   }
 
   public onChipRemove(target: string): void {
-    this.activeFilterChips.delete(target)
+    this.activeFilterChips.delete(target);
     //Retrigger filter
-    this.select.setValue(this.select.value)
+    this.select.setValue(this.select.value);
   }
 }
